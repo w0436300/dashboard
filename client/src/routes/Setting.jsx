41b769e982bcca41c5d83c4cc9f8e5dfa1c30ca7
Bowen Sun <bowen@Bowens-MacBook-Air.local>
2024-11-14 11:42:25 -0500

    modified the setting page

--- client/src/routes/Setting.jsx
+++ client/src/routes/Setting.jsx
@@ -1,10 +1,194 @@
 //setting frontend
 import React from 'react';
 
-export default function setting() {
+//export default function setting() {
+//  return (
+//    <div className="space-y-8 p-6 bg-gray-50 min-h-screen">
+//      <h1 className="text-3xl font-bold">setting </h1>
+//    </div>
+//  );
+//}
+
+
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+
+export default function Settings() {
   return (
-    <div className="space-y-8 p-6 bg-gray-50 min-h-screen">
-      <h1 className="text-3xl font-bold">setting </h1>
-    </div>
+    <form>
+      <div className="space-y-12">
+        {/* Change Password Section */}
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">Change Password</h2>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="#" method="POST" className="space-y-6">
+              <div>
+                <label htmlFor="current-password" className="block text-sm/6 font-medium text-gray-900">
+                  Current password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="current-password"
+                    name="current-password"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-transparent"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="new-password" className="block text-sm/6 font-medium text-gray-900">
+                  New password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="new-password"
+                    name="new-password"
+                    type="password"
+                    required
+                    autoComplete="new-password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-transparent"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="confirm-password" className="block text-sm/6 font-medium text-gray-900">
+                  Confirm new password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="confirm-password"
+                    name="confirm-password"
+                    type="password"
+                    required
+                    autoComplete="new-password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-transparent"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Change password
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* Profile Section */}
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">Profile</h2>
+          <p className="mt-1 text-sm/6 text-gray-600">
+            This information will be displayed publicly so be careful what you share.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label htmlFor="business-name" className="block text-sm/6 font-medium text-gray-900">
+                Business Name
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">dashboard.group2/</span>
+                  <input
+                    id="business-name"
+                    name="business-name"
+                    type="text"
+                    placeholder="janesmith"
+                    autoComplete="organization"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label htmlFor="about" className="block text-sm/6 font-medium text-gray-900">
+                About
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-transparent" 
+                  defaultValue={''}
+                />
+              </div>
+              <p className="mt-3 text-sm/6 text-gray-600">Write a few sentences about your business.</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Personal Information Section */}
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base/7 font-semibold text-gray-900">Personal Information</h2>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-transparent"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-transparent"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 bg-transparent"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm/6 font-semibold text-gray-900">
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
   );
 }
